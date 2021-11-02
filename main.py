09e97dc708bca650ee0da3c291332d4960b3aec7
lachugin <anton.lachugin.l@gmail.com>
2021-11-02 13:52:21 +0300

    skip_updates

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ from handlers import client, admin, other
 client.register_handlers_client(dp)
 other.register_handlers_other(dp)
 
-executor.start_polling(dp, on_startup=on_startup)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
